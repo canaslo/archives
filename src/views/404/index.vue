67719c2c2ac627f6654f65e34f508b73eb39e3e1
李宜珂 <18678193919@163.com>
2024-01-02 01:18:24 +0800

    404页面

---
+++ src/views/404/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-container>
+    <el-main>
+      <div class="box">
+        <img
+          src="@/assets/images/404.png"
+          draggable="false"
+          oncontextmenu="return false"
+          alt=""
+        />
+      </div>
+    </el-main>
+  </el-container>
+</template>
+<script>
+export default {
+  name: 'Default',
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+<style scoped lang='scss'>
+.el-main {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .box {
+    width: 60rem;
+  }
+}
+</style>
