3e6bfe140eb415f0f53f7fd2d5af4c28ad64ed59
李宜珂 <18678193919@163.com>
2024-01-02 01:10:47 +0800

    引入sass 依赖

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>登录页</div>
+  <div class="login">
+    <el-container>
+      <el-main>
+        测试
+      </el-main>
+    </el-container>
+  </div>
 </template>
 <script>
 export default {
@@ -10,4 +16,8 @@ export default {
   methods: {}
 }
 </script>
-<style scoped lang='scss'></style>
+<style scoped lang='scss'>
+.login {
+  height: 100%;
+}
+</style>
